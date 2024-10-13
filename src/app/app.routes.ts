328cd0c2348032a04ae9e5328f1d15b107b08e15
Gehan1226 <gehansithija2023@gmail.com>
2024-10-13 09:51:25 +0530

    add role form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ManagerLayoutComponent } from './layout/manager-layout/manager-layout.component';
 import { DashboardComponent } from './components/manager/dashboard/dashboard.component';
 import { AddEmployeeComponent } from './components/manager/add-employee/add-employee.component';
+import { AddRoleComponent } from './components/manager/add-role/add-role.component';
 
 export const routes: Routes = [
     { 
@@ -9,7 +10,8 @@ export const routes: Routes = [
         component: ManagerLayoutComponent, 
         children: [
           { path: 'dashboard', component: DashboardComponent } , 
-          { path: 'add-employee', component: AddEmployeeComponent }
+          { path: 'add-employee', component: AddEmployeeComponent } ,
+          { path: 'add-role', component: AddRoleComponent }
         ] 
       }
 ]
