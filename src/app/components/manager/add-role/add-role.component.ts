575e2fd187ef9568ee108ea02db6f3cc0bb758a0
Gehan1226 <gehansithija2023@gmail.com>
2024-10-24 07:31:35 +0530

    bind role data with form group

--- src/app/components/manager/add-role/add-role.component.ts
+++ src/app/components/manager/add-role/add-role.component.ts
@@ -1,12 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-role',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './add-role.component.html',
   styleUrl: './add-role.component.css'
 })
 export class AddRoleComponent {
+
+  public roleForm: FormGroup;
+
+  constructor(private http: HttpClient) {
+    this.roleForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required])
+    });
+  
+  }
+
+  addEmployee() {
+    console.log('Role added', this.roleForm);
+    
+  };
 
 }
